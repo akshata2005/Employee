951f678028fad0846518be3d18b087d12f82183e
akshata2005 <akshatasawant1805@gmail.com>
2022-01-14 17:12:46 +0530

    [akshata] Adding CalculateTotalWage

--- EmployeWages/EmployeWages/EmployeeWageComputation.cs
+++ EmployeWages/EmployeWages/EmployeeWageComputation.cs
@@ -12,19 +12,26 @@ namespace EmployeWages
         {
             //UC1 Program
             //constants
-            //uc1 programs
+            //uc2 Calculate totalwage of employee
             int IS_FULL_TIME = 1;
             Random random = new Random();
-            //computation
+            int totalWage = 0;
+            int empHrs = 0;
+            int empRatePerHr = 20;
+
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            totalWage = empRatePerHr * empHrs;
+            Console.WriteLine("Total Wage:" + totalWage);
 
         }
     }
