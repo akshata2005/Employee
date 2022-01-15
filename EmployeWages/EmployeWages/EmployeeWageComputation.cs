df961078c3576cece238cfddbacbef8c88b915b4
akshata2005 <akshatasawant1805@gmail.com>
2022-01-15 18:31:25 +0530

    [akshata] Adding SwitchCaseCalculatingTotalWage.cs

--- EmployeWages/EmployeWages/EmployeeWageComputation.cs
+++ EmployeWages/EmployeWages/EmployeeWageComputation.cs
@@ -8,34 +8,38 @@ namespace EmployeWages
 {
     public class EmployeeWageComputation
     {
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
         static void Main(string[] args)
         {
             //UC1 Program
             //constants
            
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME=2;
-            int EMP_RATE_PER_HR = 20;
             //variables
             int totalWage = 0;
             int empHrs = 0;
             Random random = new Random();
 
             int randomInput = random.Next(0, 3);
-            if (randomInput == IS_FULL_TIME)
+            switch (randomInput)
             {
-                Console.WriteLine("Fulltime Employee is Present");
-                empHrs = 8;
-            }
-            else if (randomInput == IS_PART_TIME)
-            {
-                Console.WriteLine("Parttime Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case  IS_FULL_TIME:
+                     Console.WriteLine("Fulltime Employee is Present");
+                     empHrs = 8;
+                     break;
+
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("Parttime Employee is Present");
+                    
+                    break;
+
+                default:
+                     Console.WriteLine("Employee is Absent");
+                     empHrs = 0;
+                    break;
             }
             //calculation of salary
             totalWage = EMP_RATE_PER_HR * empHrs;
