24c8f9777c91ffd1ca07f05144b15c1b59e180c8
akshata2005 <akshatasawant1805@gmail.com>
2022-01-25 20:07:05 +0530

    [akshata] adding witchcase

---
+++ EmployeWages/EmployeWages/CalculateEmpWage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWages
+{
+    public class CalculateEmpWage
+    {
+
+        /* Uc - 8 Implementation
+         * In this branch, Computing Wage For a Employee 
+         * of multiple Companies.
+         */
+        //Constant Variables.
+        const int FULL_TIME = 1;
+        const int PART_TIME = 2;
+
+        //static variable
+        public static int emphrs;
+
+        // method to perform Employee Wage Computation program using parameters.
+        public static void WageCompute(string company, int wagePrHrs, int totalWorkHrs, int totalWorkDay)
+        {
+            //Local Variables
+            int totalWage = 0;
+            int totalEmpWrkHr = 0;
+            int totalEmpwrkDay = 1;
+            //Creating Object of Random Class
+            Random randomNum = new Random();
+            //Checking condition.           
+            while (totalEmpWrkHr <= totalWorkHrs && totalEmpwrkDay <= totalWorkDay)
+            {
+                int empCheck = randomNum.Next(0, 3);     //generating random number from 0 to 2.
+                GetEmpHrs(empCheck);                    //calling static method to get emp work hr.
+                int empWage = emphrs * wagePrHrs;
+                Console.WriteLine("Emp wage for day{0} is: {1}", totalEmpwrkDay, empWage);
+                totalWage += empWage;
+                totalEmpWrkHr = emphrs + totalEmpWrkHr;      //Computing Total Work Hrs of Employee Day wise.
+                totalEmpwrkDay++;                           //incrementing Number of Day Worked.
+            }
+            Console.WriteLine("\nEmployee of company:{0} and Total wage is:{1} ", company, totalWage);
+        }
+
+        //Method to Get Employee work hours.
+        public static void GetEmpHrs(int empCheck)
+        {
+            switch (empCheck)       //passing random number into switch to get employee work hours.
+            {
+                case FULL_TIME:
+                    emphrs = 8;
+                    break;
+                case PART_TIME:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+        }
+    }
+}
--- EmployeWages/EmployeWages/EmployeeWageComputation.cs
+++ EmployeWages/EmployeWages/EmployeeWageComputation.cs
@@ -8,46 +8,15 @@ namespace EmployeWages
 {
     public class EmployeeWageComputation
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-        static void computeEmpWage(String company, int empRatePerHrs, int numOfWorkingDays, int maxHoursPerMonth)
+        // Main Method /Entry point
+        static void Main(string[] args)
         {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + "emp Hrs:" + empHrs);
-            }
-
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage for Company  " + company + " is : " + totalEmpWage);
-
-        }
-        public static void Main()
-        {
-            computeEmpWage("NOKIA", 20, 2, 10);
-            computeEmpWage("SAMSUNG", 10, 4, 20);
+            //printing message on console
+            Console.WriteLine("Welcome To Employee Wage Computation Program \n");
+            CalculateEmpWage.WageCompute("Dmart", 30, 120, 25);    //calling static Method with parameters.
+            CalculateEmpWage.WageCompute("Reliance", 25, 125, 24);
+            CalculateEmpWage.WageCompute("Amazon", 40, 110, 22);
+            Console.ReadLine();
         }
     }
 }
