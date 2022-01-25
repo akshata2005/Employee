33a81e53c3af212adf7cc59882ad1d19d9c6970b
akshata2005 <akshatasawant1805@gmail.com>
2022-01-25 19:54:17 +0530

    [akshata] created the employee company

--- EmployeWages/EmployeWages/EmployeeWageComputation.cs
+++ EmployeWages/EmployeWages/EmployeeWageComputation.cs
@@ -8,51 +8,46 @@ namespace EmployeWages
 {
     public class EmployeeWageComputation
     {
-
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int MAX_WORKING_DAYS = 20;
-        public const int MAX_HOURS_IN_MONTH = 100;
-        static void Main(string[] args)
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        static void computeEmpWage(String company, int empRatePerHrs, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            //UC1 Program
-            //constants
-           
-            //variables
-            int totalEmpHrs = 0;
             int empHrs = 0;
+            int totalEmpHrs = 0;
             int totalWorkingDays = 0;
-            while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays <MAX_WORKING_DAYS)
+            while (empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
-                int randomInput = random.Next(0, 3);
-                switch (randomInput)
-                {
-                    case IS_FULL_TIME:
-                       // Console.WriteLine("Fulltime Employee is Present");
-                        empHrs = 8;
-                        break;
 
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
                     case IS_PART_TIME:
                         empHrs = 4;
-                       // Console.WriteLine("Parttime Employee is Present");
-
                         break;
-
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
                     default:
-                       // Console.WriteLine("Employee is Absent");
                         empHrs = 0;
                         break;
                 }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "emp Hrs:" + empHrs);
             }
-           
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Day#" + totalWorkingDays + "Emp Hrs" + empHrs);
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Total emp wage :",  + totalEmpWage);
-            Console.ReadLine();
+
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage for Company  " + company + " is : " + totalEmpWage);
+
+        }
+        public static void Main()
+        {
+            computeEmpWage("NOKIA", 20, 2, 10);
+            computeEmpWage("SAMSUNG", 10, 4, 20);
         }
     }
 }
